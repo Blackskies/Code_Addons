c79038a9ec82eab02c0d646afc698dd7fc53cef6
gumpena_ravi <gmpn1996@gmail.com>
2017-09-28 22:03:24 +0530

    Added Epoch time 2 readable Time

--- epoch2time.py
+++ epoch2time.py
@@ -1,8 +1,31 @@
+"""
+				CONVERTING EPOCH TIME TO GENERAL READABLE TIME MODULE
+"""
 # == Imports ===================================================================
 import datetime
 # == Input Time ================================================================
-time = 1506065040
 
-value = datetime.datetime.fromtimestamp(time)
-print(value.strftime('%Y-%m-%d %H:%M:%S'))
+def epoch2time(time):
+	"""
+	This converts epoch time to normal time 
+
+	Parameters
+	----------
+	time : Integer
+		Epoch Time
+
+	Returns
+	-------
+	normal : String
+		General Time
+	"""
+	value = datetime.datetime.fromtimestamp(time)
+	Normal = value.strftime('%Y-%m-%d %H:%M:%S')
+	print(normal)
+	return normal
+
+if __name__ == '__main__':
+	time = 1506065040 #input sample
+	epoch2time(time)
+	
 # == END =======================================================================
